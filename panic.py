58f365fab82c320e98e34d281e6be21997204c74
gminafuzz <milenium585@mail.com>
2018-10-14 23:56:59 +0300

    first exsercise

--- panic.py
+++ panic.py
@@ -12,3 +12,16 @@ plist.insert(2, plist.pop(3))
 new_phrase = ' '.join(plist)
 print(plist)
 print(new_phrase)
+
+# Ver. 2. With slice
+
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+
+new_phrase = ''.join(plist[1:3])
+new_phrase = new_phrase + ''.join([ plist[5], plist[4], plist[7], plist[6] ])
+
+print(plist)
+print(new_phrase)
